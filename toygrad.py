af28ab49631c29d8384452a3417ccc76cedf0085
sytandas <sytandas@gmail.com>
2026-02-16 19:43:17 +0530

    rename

--- toygrad.py
+++ toygrad.py
@@ -1,18 +1,19 @@
 # input processing of value and gradient
+
 class Value:
-    def __init___(self, data):
-        self.data = data 
-        print(data)
-#    def __repr__(self):
-#        return f"value = {self.data}"
-#        print(data)
+    def __init__(self):
+        self.data
+
+    def __repr__(self):
+        return f"value = {self.data}"
+
     def __add__(self, other_value): # a.__add__(b)
         return(self.data + other_value.data) 
-        print(data)
 
     def __mul__(self, other_value): # a.__add__(b)
         return(self.data * other_value)
 
+# gredient
 a = 2.0
 b = -1
 c = 10
