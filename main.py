57c3573b5af9068fbfd67836c49982f8984a8644
sytandas <sytandas@gmail.com>
2026-05-12 22:38:45 +0530

    in

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 import torch
 
 x = torch.tensor(2.0, requires_grad=True)
-y = x**2 + 3*x + 1   # y = x² + 3x + 1
+y = x**2 + 3*x + 1   
 y.backward()         # compute dy/dx
 print(x.grad)        # dy/dx = 2x + 3 → 7
